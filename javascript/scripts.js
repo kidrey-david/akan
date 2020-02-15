58e7eaa0d7e956e64bff91b2bcc3bf6c18785d39
david <neymardavid21@gmail.com>
2020-02-15 16:07:35 +0300

    js update

---
+++ javascript/scripts.js
@@ -0,0 +1,11 @@
+function akanName(CC,YY,MM,DD) {
+    var date=["Sunday", "Monday", "Tuesday", "Wednesday", "Thurdsay", "Friday", "Saturday"];
+    var male=["Kwasi", "Kwadwo", "Kwaben", "Kwaku", "Yaw", "Kofi","Kwame"];
+    var female=["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"];
+    var DD= parseInt(document.getElementById("siku").value);
+    var MM=parseInt(document.getElementById("mwezi").value);
+    var year=parseInt(document.getElementById("mwaka").value);
+    var YY=parseInt(year.slice(2,4));
+    var CC=parseInt(year.slice(0,2));
+    var  
+}
