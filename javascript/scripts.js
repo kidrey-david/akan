47c4824e43ab63115533d23159a104f41c72144b
david <neymardavid21@gmail.com>
2020-02-15 22:35:56 +0300

    changes made

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -7,5 +7,16 @@ function akanName(CC,YY,MM,DD) {
     var year=parseInt(document.getElementById("mwaka").value);
     var YY=parseInt(year.slice(2,4));
     var CC=parseInt(year.slice(0,2));
-    var  
+    var gender=document.getElementById("gender").value;
+    var arrayPosition= Math.abs((((CC/4)-2*CC-1)+((5*YY/4)) + ((26*(MM+1)/10))+DD)%7);
+    arrayPosition= Math.floor(arrayPosition);
+    var sex
+    if(gender === "male"){
+        sex=male;
+    } else {
+        sex = female;
+    };
+    var name= sex[arrayPosition];
+    date= date[arrayPosition];
+    document.getElementById("akanName").innerHTML="Your Akan name is" + name + "and you were born in" + date
 }
