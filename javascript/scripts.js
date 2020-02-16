0c2d240a08880ced03b6feed9d9b02c61559bbcf
david <neymardavid21@gmail.com>
2020-02-16 16:53:38 +0300

    updated js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,22 +1,35 @@
-function akanName(CC,YY,MM,DD) {
-    var date=["Sunday", "Monday", "Tuesday", "Wednesday", "Thurdsay", "Friday", "Saturday"];
-    var male=["Kwasi", "Kwadwo", "Kwaben", "Kwaku", "Yaw", "Kofi","Kwame"];
-    var female=["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"];
-    var DD= parseInt(document.getElementById("siku").value);
-    var MM=parseInt(document.getElementById("mwezi").value);
-    var year=parseInt(document.getElementById("mwaka").value);
-    var YY=parseInt(year.slice(2,4));
-    var CC=parseInt(year.slice(0,2));
-    var gender=document.getElementById("gender").value;
-    var arrayPosition= Math.abs((((CC/4)-2*CC-1)+((5*YY/4)) + ((26*(MM+1)/10))+DD)%7);
-    arrayPosition= Math.floor(arrayPosition);
-    var sex
-    if(gender === "male"){
-        sex=male;
-    } else {
-        sex = female;
-    };
-    var name= sex[arrayPosition];
-    date= date[arrayPosition];
-    document.getElementById("akanName").innerHTML="Your Akan name is" + name + "and you were born in" + date
+function ghanaName(){
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var dayOfMonth = document.getElementById("day").value;
+    var date = new Date (`${year}-${month}-${dayOfMonth}`);
+    var birthDay = date.getDay();
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    var result = document.getElementById("result");
+    var days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Kwabena", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "AKua", "Yaa", "Afua", "Ama"];
+
+    if (year == "" || year.length < 4 || year.length > 4){
+        alert("Hey you need to enter a valid year!");
+    }
+    else if (month < 0 || month > 12 ){
+        alert("Hey you need to enter a valid month!");
+    }
+    else if( dayOfMonth < 0 || dayOfMonth >31) {
+        alert("Hey you need to re-enter a valid day!");
+    }
+    else if (male.checked == false && female.checked == false){
+        alert("You need to specify your gender!");
+    }
+    else if (male.checked == true){
+        result.innerHTML = "Your Ghanaian name is " + maleNames[birthDay] +  " since you were born on a " + days[birthDay];
+    } 
+    else if (female.checked == true){
+        result.innerHTML = "Your Ghanaian name is " + femaleNames[birthDay] +  " since you were born on a " + days[birthDay];
+    }
+    else{
+        alert("Come on Akan, just run!")
+    }
 }
